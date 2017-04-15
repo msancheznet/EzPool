c9ea1ea94fbbfa509c85111b7693e2169c926480
DESKTOP\msanc <msancheznet@gmail.com>
2017-04-15 16:57:39 -0400

    minor edits

--- pyro_example/worker.py
+++ pyro_example/worker.py
@@ -12,14 +12,14 @@ class FibWorker(Worker):
 		print(result)
 		return result
 
-def _get_args():
+def _get_args_parser():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--address','-a', nargs='?', default='localhost', help='IP adress', type=str)
 	parser.add_argument('--port','-p', nargs='?', default=20000, help='TCP port', type=int)
 	parser.add_argument('--name','-n', nargs='?', default='worker', help='Worker name', type=str)
 	parser.add_argument('--msg','-m', nargs='?', default='Worker ready to proces jobs:', help='Message to display when object is started', type=str)
 	#parser.add_argument('--nserver','-ns', action='store_true', help='Run name server')
-	return parser.parse_args()
+	return parser
 
 if __name__ == '__main__':
-	run_object(FibWorker, _get_args())
+	run_object(FibWorker, _get_args_parser().parse_args())
