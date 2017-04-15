8ee3293220c07194b456ea31a67416f53e1a6430
DESKTOP\msanc <msancheznet@gmail.com>
2017-04-15 17:00:06 -0400

    Modify the DistributedPool to be able to close workers and shutdown all of them at once. This requies adding a lock to ensure that operations that modify workers are thread-safe

--- pyro_example/scheduler.py
+++ pyro_example/scheduler.py
@@ -3,12 +3,12 @@ import Pyro4
 import sys
 from copy import copy
 from functools import wraps
-from threading import Thread
+from threading import Thread, Lock
 if sys.version_info >= (3, 0):
 	from queue import Queue
 else:
 	from Queue import Queue
-from objects import EzProxy
+from objects import EzProxy, Closeable, run_object
 
 class WorkerQueue(Queue):
 	''' Subclass Python's native queue to work with Pyro's proxys. `WorkerQueue`
@@ -30,22 +30,44 @@ class WorkerQueue(Queue):
 
 				:param str uri: A fully formed URI address
 		'''
-		print('Hola')
 		proxy = EzProxy(uri)
 		if not proxy.connected:
 			print('Worker {} is not available'.format(uri))
 			return
-		print('Hola2')
 
 		if not proxy.is_worker:
 			Print('Proxy {} is not a compatible worker. You need to subclass Worker'.format(uri))
 			return
-		print('Hola3')
 
 		print('Worker {} is available'.format(uri))
 		self._uris.add(uri)
 		Queue._put(self, proxy)
-		print('Hola4')
+
+	def __getitem__(self, uri):
+		""" Find an item within this queue as indexed by its URI.
+
+			.. Warning:: You should only use this feature from within the DistributedPool
+						 along with its *mutex*. Otherwise, this is not thread-safe
+
+			:param str uri: URI of the worker to access
+			:return Proxy: Proxy to the worker or None if empty
+		"""
+		# The queue is empty, so return None
+		if self.qsize() == 0:
+			return
+
+		# Iterate through the queue grabbing a worker, comparing its URI with the one provided
+		# and putting it back if they do not match. Note that this implementation assumes that
+		# all workers are equal and therefore their order does not matter
+		found = None
+		for i in range(self.qsize()):
+			p = self.get_nowait()
+			if p.uri == uri:
+				found = p
+				self._uris.remove(uri)
+				break
+			self.put_nowait(p)
+		return found
 
 	def __contains__(self, uri):
 		""" Reimplement the `contains` operation to check if a given
@@ -60,12 +82,13 @@ class WorkerQueue(Queue):
 		return self.qsize()
 
 	def __bool__(self):
+		""" Return true if the queue is not empty """
 		return not self.empty()
 
 
 @Pyro4.expose
 @Pyro4.behavior(instance_mode="single")
-class DistributedPool(object):
+class DistributedPool(Closeable):
 	''' Distributed pool of workers analogous to Python's multiprocessing Pool.
 
 			:ivar WorkerQueue _idle: Queue with idle workers
@@ -108,18 +131,25 @@ class DistributedPool(object):
 									 concurrently running is equal to `min(# tasks, # workers)`, so this
 									 sets a limitation in the number of tasks that the pool can support
 	'''
+	def __init__(self, daemon):
+		Closeable.__init__(self, daemon)
 
-	def __init__(self):
 		'''Initialize the pool.'''
 		self._idle = WorkerQueue()
 		self._running = {}
 		self._results = {}
 
+		# The mutex must be held to change the pool of workers
+		self._mutex = Lock()
+
+		# Indicate that this pool has been shutdown
+		self._shut = False
+
 	@property
-	def _workers(self):
+	def workers(self):
 		'''	Get workers available in this pool '''
 		uris = copy(self._idle._uris)
-		uris.update(self._running.keys())
+		uris.update(self._running.keys())	# In reality this is not necessary, no one takes URIs out of _idle queue
 		return set(uris)
 
 	def add_worker(self, uri):
@@ -127,8 +157,55 @@ class DistributedPool(object):
 
 				:param str uri: The URI of the worker to register
 		'''
-		if uri not in self._workers:
-			self._idle.add(uri)
+		with self._mutex:
+			if uri not in self.workers:
+				self._idle.add(uri)
+
+	def close_worker(self, uri):
+		""" Close a worker in this pool.
+
+			.. Tip:: This function is thread-safe, so use it for your operations
+
+			:param str URI: Worker to close
+		"""
+		with self._mutex:
+			self._close_worker(uri)
+
+	def _close_worker(self, uri):
+		""" Close the worker identified by the passed URI
+ 			
+ 			:param str URI: Worker to close
+		"""
+		if uri in self._running:
+			# If worker is running, it can be accessed directly
+			print('WARNING: Worker {} is shutting down while still processing a job'.format(uri))
+			proxy = self._running.pop(uri)
+			self._shutdown_proxy(proxy)
+		else:
+			# Get proxy from the idle queue of workers
+			proxy = self._idle[uri]
+			self._shutdown_proxy(proxy)
+
+		print('Worker {} has been closed'.format(uri))
+
+	def shutdown(self):
+		""" Shutdown the pool and all its workers """
+		with self._mutex:
+			for uri in self.workers:
+				self._close_worker(uri)
+			self._shut = True
+
+	def _shutdown_proxy(self, proxy):
+		""" Shutdown a proxy and release the connection 
+			
+			.. Warning:: You should only call this function with you have the mutex
+		"""
+		if proxy is None:
+			print('Worker {} is not present in the pool'.format(uri))
+			return
+		proxy.shutdown()
+		proxy._pyroRelease()
+		del proxy
 
 	def __contains__(self, uri):
 		''' Check if the worker identified by this URI is already in the pool
@@ -136,9 +213,10 @@ class DistributedPool(object):
 				:param str uri: The worker URI
 				:param bool: True if the worker is already in the pool
 		'''
-		return (uri in self._workers)
+		return (uri in self.workers)
 
 	def __len__(self):
+		""" Return the number of workers available """
 		return len(self._idle) + len(self._running)
 
 	def map(self, tasks):
@@ -147,7 +225,7 @@ class DistributedPool(object):
 				results are available for collecting them.
 
 				:param list tasks: List containing the inputs for each function evaluation.
-
+	
 				.. Tip:: If the function evaluated by the distributed system has more than one input,
 								 pass a list of tuples, one per task to be run
 				.. Warning:: The current `map` implementation does not support keyword arguments
@@ -162,18 +240,25 @@ class DistributedPool(object):
 
 		# Submit the tasks, running each of them in a separate thread
 		for task in tasks:
-			# Get the next available worker. If all of them are busy, block for
-			# now
-			worker = self._idle.get(block=True)
-			wrk_id = worker._pyroUri.asString()
+			# Acquire the mutex so that you cannot add/delete worker while you
+			# are using it here
+			with self._mutex:
+				# If this pool is shut, do not submit any more tasks
+				if self._shut:
+					break
 
-			# Initialize the thread and mark this worker as running
-			# Note: Do not start the thread before putting it into _running to
-			# prevent concurrency problems
-			thread = Thread(target=self._run_task, args=(wrk_id, task))
-			threads.append(thread)
-			self._running[wrk_id] = worker
-			thread.start()
+				# Get the next available worker. If all of them are busy, block for
+				# now
+				worker = self._idle.get(block=True)
+				wrk_id = worker.uri
+
+				# Initialize the thread and mark this worker as running
+				# Note: Do not start the thread before putting it into _running to
+				# prevent concurrency problems
+				thread = Thread(target=self._run_task, args=(wrk_id, task))
+				threads.append(thread)
+				self._running[wrk_id] = worker
+				thread.start()
 
 		# Wait for all the threads that monitor workers to finish. This ensures that
 		# all results are available when we return from this function
@@ -184,13 +269,17 @@ class DistributedPool(object):
 
 	def _run_task(self, wrk_id, task):
 		''' Run a task by calling the `run` method in the worker. Note that this function
-				is called always from a separate thread.
+			is called always from a separate thread.
 
-				:param str wrk_id: Worker URI that will run the task
-				:param tuple task: Tuple with the inputs for the `run` function in the workers, properly ordered
+			:param str wrk_id: Worker URI that will run the task
+			:param tuple task: Tuple with the inputs for the `run` function in the workers, properly ordered
 		'''
-		# Get the worker that will perform the task
-		worker = self._running[wrk_id]
+		# Get the worker that will perform the task. If not present, this means that another
+		# concurrent task deleted it.
+		worker = self._running.get(wrk_id, None)
+		if worker is None:
+			self._results[task] = 'Worker was unavailable at evaluation time'
+			return
 
 		# Wrap the task with a try catch to silently fail if something goes wrong in the worker.
 		# The user will see that, for a given task, the map has returned an
@@ -206,34 +295,19 @@ class DistributedPool(object):
 		# available
 		self._idle.put_nowait(worker)
 
-
-def run_pool(args):
-	'''	Run the distributed pool as a basic Pyro4 server
-
-			:param ArgumentParser args: Set of arguments parsed
-
-			.. Tip:: The native Python argument parser is used to standardize the inputs
-	'''
-	Pyro4.Daemon.serveSimple({DistributedPool: args.name}, host=args.address,
-							 port=args.port, ns=args.nserver)
-
-
-def _get_arg_parser():
+def _get_args_parser():
 	'''	Create the argument parser that defines the set of options available to configure
 			a distributed pool
 
 			.. Tip:: To see options available, locate this file in a command shell and execute `python distributed_pool.py -h`
 	'''
 	parser = argparse.ArgumentParser()
-	parser.add_argument('--address', '-a', nargs='?',
-						default='localhost', help='IP adress', type=str)
-	parser.add_argument('--port', '-p', nargs='?',
-						default=21000, help='TCP port', type=int)
-	parser.add_argument('--name', '-n', nargs='?',
-						default='pool', help='Distributed pool name', type=str)
-	parser.add_argument('--nserver', '-ns',
-						action='store_true', help='Run with name server')
+	parser.add_argument('--address', '-a', nargs='?',default='localhost', help='IP adress', type=str)
+	parser.add_argument('--port', '-p', nargs='?',default=21000, help='TCP port', type=int)
+	parser.add_argument('--name', '-n', nargs='?',default='pool', help='Distributed pool name', type=str)
+	parser.add_argument('--msg','-m', nargs='?', default='Distributed pool ready to proces jobs:', help='Message to display when object is started', type=str)
+	#parser.add_argument('--nserver', '-ns',action='store_true', help='Run with name server')
 	return parser
 
 if __name__ == '__main__':
-	run_pool(_get_arg_parser().parse_args())
+	run_object(DistributedPool, _get_args_parser().parse_args())
